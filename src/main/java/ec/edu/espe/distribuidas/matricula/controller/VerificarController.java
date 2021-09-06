fa94708c07d66b9579cae8d43c9ce7bfa5b6d4dd
baromero6 <baromero6@espe.edu.ec>
2021-09-06 12:50:08 -0500

    check validation

---
+++ src/main/java/ec/edu/espe/distribuidas/matricula/controller/VerificarController.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2021 bran-.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    bran- - initial API and implementation and/or initial documentation
+ */
+package ec.edu.espe.distribuidas.matricula.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author bran-
+ */
+@RestController
+@RequestMapping("/v1/check")
+public class VerificarController {
+
+    @GetMapping
+    public ResponseEntity funcionamiento() {
+        return ResponseEntity.ok().body("Fucnionando 2");
+    }
+
+}
